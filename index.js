5f8c2f3b759451b6a5ed9f82161039b2cd3f6b36
sergey <110@mobilon.ru>
2022-02-19 01:11:52 +0700

    fix

--- index.js
+++ index.js
@@ -1,247 +1,209 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const axios = require('axios');
-
 class GupshupAPIClient {
-  constructor ({API_KEY, APP_NAME, SOURCE_MOBILE_NUMBER}) {
-    this.API_KEY = API_KEY;
-    this.APP_NAME = APP_NAME;
-    this.SOURCE_MOBILE_NUMBER = SOURCE_MOBILE_NUMBER;
-
-    this.url = {
-      getTemplatesList: `https://api.gupshup.io/sm/api/v1/template/list/${APP_NAME}`,
-      optInUser: `https://api.gupshup.io/sm/api/v1/app/opt/in/${APP_NAME}`,
-      bulkOptIn: `https://api.gupshup.io/sm/api/v1/app/opt/in/${APP_NAME}`,
-      sendTextMessage: 'https://api.gupshup.io/sm/api/v1/msg',
-      sendTemplateMessage: 'http://api.gupshup.io/sm/api/v1/template/msg',
-      getWalletBalance: 'https://api.gupshup.io/sm/api/v2/wallet/balance',
-    };
-
-    this.config = {
-      headers: {
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        apiKey: this.API_KEY
-      }
-    };
-  };
-
-  /**
-   * 
-   * @param {*} data 
-   * @returns 
-   */
-  getUrlEncodedData = (data) => {
-    const resultantData = new URLSearchParams();
-    Object.keys(data).forEach(key => {
-      resultantData.append(key, typeof data[key] === 'object' ? JSON.stringify(data[key]) : data[key]);
-    });
-    return resultantData;
-  };
-
-  /**
-   * 
-   * @returns 
-   */
-  getTemplatesList = async () => await axios.get(this.url.getTemplatesList, this.config);
-
-  getWalletBalance = async () => await axios.get(this.url.getWalletBalance, this.config);
-
-  markUserOptIn = async (userMobileNumber) => {
-    const params = this.getUrlEncodedData({
-      user: userMobileNumber
-    });
-
-    return await axios.post(this.url.optInUser, params, this.config);
-  };
-
-  markBulkOptIn = async (userMobileNumbers) => {
-    const params = this.getUrlEncodedData({
-      users: userMobileNumbers
-    });
-
-    return await axios.post(this.url.bulkOptIn, params, this.config);
-  };
-
-  sendMediaImageMessage = async (userMobileNumber, imageUrl, caption) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      'src.name': this.APP_NAME,
-      message: {
-        type: 'image',
-        originalUrl: imageUrl,
-        previewUrl: imageUrl,
-        caption
-      }
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendMediaVideoMessage = async (userMobileNumber, videoUrl, caption) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'video',
-        url: videoUrl,
-        caption,
-      },
-      'src.name': this.APP_NAME
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendMediaAudioMessage = async (userMobileNumber, audioUrl) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'audio',
-        url: audioUrl,
-      },
-      'src.name': this.APP_NAME
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendMediaFileMessage = async (userMobileNumber, fileUrl, filename) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'file',
-        url: fileUrl,
-        filename,
-      },
-      'src.name': this.APP_NAME
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-  
-  sendMediaStickerMessage = async (userMobileNumber, stickerUrl) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'sticker',
-        url: stickerUrl,
-      },
-      'src.name': this.APP_NAME
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendTextMessage = async (userMobileNumber, message) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'text',
-        text: message
-      },
-      'src.name': this.APP_NAME,
-      disablePreview: false
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendTemplateMessage = async (userMobileNumber, templateId, templateParams, mediaMessage) => {
-    const params = this.getUrlEncodedData({
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      template: {
-        id: templateId,
-        params: templateParams
-      },
-      message: mediaMessage
-    });
-
-    return await axios.post(this.url.sendTemplateMessage, params, this.config);
-  };
-
-  sendLocation = async (userMobileNumber, longitude, latitude, name, address) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'location',
-        longitude,
-        latitude,
-        name,
-        address,
-      },
-      'src.name': this.APP_NAME,
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendLocation = async (userMobileNumber, longitude, latitude, name, address) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'location',
-        longitude,
-        latitude,
-        name,
-        address,
-      },
-      'src.name': this.APP_NAME,
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  sendContactCard = async (userMobileNumber, contactCard) => {
-    const params = this.getUrlEncodedData({
-      channel: 'whatsapp',
-      source: this.SOURCE_MOBILE_NUMBER,
-      destination: userMobileNumber,
-      message: {
-        type: 'contact',
-        contact: contactCard,
-      },
-      'src.name': this.APP_NAME,
-    });
-
-    return await axios.post(this.url.sendTextMessage, params, this.config);
-  };
-
-  checkContentType = (type, contentType) => {
-    const types = {
-      audio: ['audio/aac', 'audio/mp4', 'audio/amr', 'audio/mpeg', 'audio/ogg;codecs=opus'],
-      image: ['image/jpeg', 'image/png'],
-      video: ['video/mp4', 'video/3gpp'],
-    };
-    return types[type].includes(contentType.replace(/ /g,''));
-  }
-
-  checkSize = (type, size) => {
-    const types = {
-      image: 5 * 1024 * 1024,  // 5mb
-      audio: 16 * 1024 * 1024, // 16mb      
-      video: 16 * 1024 * 1024, // 16mb
-      file: 100 * 1024 * 1024, // 100mb
-      sticker: 100 * 1024,     // 100kb
+    constructor({ API_KEY, APP_NAME, SOURCE_MOBILE_NUMBER }) {
+        /**
+         *
+         * @param {*} data
+         * @returns
+         */
+        this.getUrlEncodedData = (data) => {
+            const resultantData = new URLSearchParams();
+            Object.keys(data).forEach((key) => {
+                return resultantData.append(key, typeof data[key] === 'object' ? JSON.stringify(data[key]) : data[key]);
+            });
+            return resultantData;
+        };
+        /**
+         *
+         * @returns
+         */
+        this.getTemplatesList = () => __awaiter(this, void 0, void 0, function* () { return yield axios.get(this.url.getTemplatesList, this.config); });
+        this.getWalletBalance = () => __awaiter(this, void 0, void 0, function* () { return yield axios.get(this.url.getWalletBalance, this.config); });
+        this.markUserOptIn = (userMobileNumber) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                user: userMobileNumber
+            });
+            return yield axios.post(this.url.optInUser, params, this.config);
+        });
+        this.markBulkOptIn = (userMobileNumbers) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                users: userMobileNumbers
+            });
+            return yield axios.post(this.url.bulkOptIn, params, this.config);
+        });
+        this.sendMediaImageMessage = (userMobileNumber, imageUrl, caption) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                'src.name': this.APP_NAME,
+                message: {
+                    type: 'image',
+                    originalUrl: imageUrl,
+                    previewUrl: imageUrl,
+                    caption
+                }
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendMediaVideoMessage = (userMobileNumber, videoUrl, caption) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'video',
+                    url: videoUrl,
+                    caption,
+                },
+                'src.name': this.APP_NAME
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendMediaAudioMessage = (userMobileNumber, audioUrl) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'audio',
+                    url: audioUrl,
+                },
+                'src.name': this.APP_NAME
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendMediaFileMessage = (userMobileNumber, fileUrl, filename) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'file',
+                    url: fileUrl,
+                    filename,
+                },
+                'src.name': this.APP_NAME
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendMediaStickerMessage = (userMobileNumber, stickerUrl) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'sticker',
+                    url: stickerUrl,
+                },
+                'src.name': this.APP_NAME
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendTextMessage = (userMobileNumber, message) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'text',
+                    text: message
+                },
+                'src.name': this.APP_NAME,
+                disablePreview: false
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendTemplateMessage = (userMobileNumber, templateId, templateParams, mediaMessage) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                template: {
+                    id: templateId,
+                    params: templateParams
+                },
+                message: mediaMessage
+            });
+            return yield axios.post(this.url.sendTemplateMessage, params, this.config);
+        });
+        this.sendLocation = (userMobileNumber, longitude, latitude, name, address) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'location',
+                    longitude,
+                    latitude,
+                    name,
+                    address,
+                },
+                'src.name': this.APP_NAME,
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.sendContactCard = (userMobileNumber, contactCard) => __awaiter(this, void 0, void 0, function* () {
+            const params = this.getUrlEncodedData({
+                channel: 'whatsapp',
+                source: this.SOURCE_MOBILE_NUMBER,
+                destination: userMobileNumber,
+                message: {
+                    type: 'contact',
+                    contact: contactCard,
+                },
+                'src.name': this.APP_NAME,
+            });
+            return yield axios.post(this.url.sendTextMessage, params, this.config);
+        });
+        this.checkContentType = (type, contentType) => {
+            const types = {
+                audio: ['audio/aac', 'audio/mp4', 'audio/amr', 'audio/mpeg', 'audio/ogg;codecs=opus'],
+                image: ['image/jpeg', 'image/png'],
+                video: ['video/mp4', 'video/3gpp'],
+            };
+            return types[type].includes(contentType.replace(/ /g, ''));
+        };
+        this.checkSize = (type, size) => {
+            const types = {
+                image: 5 * 1024 * 1024,
+                audio: 16 * 1024 * 1024,
+                video: 16 * 1024 * 1024,
+                file: 100 * 1024 * 1024,
+                sticker: 100 * 1024, // 100kb
+            };
+            return size > 0 && size < types[type];
+        };
+        this.API_KEY = API_KEY;
+        this.APP_NAME = APP_NAME;
+        this.SOURCE_MOBILE_NUMBER = SOURCE_MOBILE_NUMBER;
+        this.url = {
+            getTemplatesList: `https://api.gupshup.io/sm/api/v1/template/list/${APP_NAME}`,
+            optInUser: `https://api.gupshup.io/sm/api/v1/app/opt/in/${APP_NAME}`,
+            bulkOptIn: `https://api.gupshup.io/sm/api/v1/app/opt/in/${APP_NAME}`,
+            sendTextMessage: 'https://api.gupshup.io/sm/api/v1/msg',
+            sendTemplateMessage: 'http://api.gupshup.io/sm/api/v1/template/msg',
+            getWalletBalance: 'https://api.gupshup.io/sm/api/v2/wallet/balance',
+        };
+        this.config = {
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                apiKey: this.API_KEY
+            }
+        };
     }
-    return size > 0 && size < types[type];
-  }
+    ;
 }
-
 module.exports = {
-  GupshupAPIClient,
-}
+    GupshupAPIClient,
+};
---
+++ index.ts
@@ -0,0 +1,299 @@
+const axios = require('axios');
+
+interface Address {
+  city: string;
+  country: string;
+  countryCode: string;
+  state: string;
+  street: string;
+  type: string;
+  zip: string;
+}
+
+interface Email {
+  email: string;
+  type: string;
+}
+
+interface Name {
+  firstName: string;
+  formattedName: string;
+  lastName: string;
+}
+
+interface Phone {
+  phone: string;
+  type: string;
+  wa_id: string;
+}
+
+interface ContactCard {
+  name: Name;
+  addresses: Address[];
+  emails: Email[];
+  phones: Phone[];
+}
+
+interface GupshupAPIClientConfig {
+  API_KEY: string;
+  APP_NAME: string;
+  SOURCE_MOBILE_NUMBER: string;
+}
+
+class GupshupAPIClient {
+  API_KEY: string;
+  APP_NAME: string;
+  SOURCE_MOBILE_NUMBER: string;  
+  url: { 
+    getTemplatesList: string; 
+    optInUser: string; 
+    bulkOptIn: string; 
+    sendTextMessage: string;
+    sendTemplateMessage: string; 
+    getWalletBalance: string; 
+  };
+  config: { 
+    headers: { 
+      'Cache-Control': string; 
+      'Content-Type': string; 
+      apiKey: any; 
+    };
+  };
+
+  constructor ({API_KEY, APP_NAME, SOURCE_MOBILE_NUMBER}: GupshupAPIClientConfig) {
+    this.API_KEY = API_KEY;
+    this.APP_NAME = APP_NAME;
+    this.SOURCE_MOBILE_NUMBER = SOURCE_MOBILE_NUMBER;
+
+    this.url = {
+      getTemplatesList: `https://api.gupshup.io/sm/api/v1/template/list/${APP_NAME}`,
+      optInUser: `https://api.gupshup.io/sm/api/v1/app/opt/in/${APP_NAME}`,
+      bulkOptIn: `https://api.gupshup.io/sm/api/v1/app/opt/in/${APP_NAME}`,
+      sendTextMessage: 'https://api.gupshup.io/sm/api/v1/msg',
+      sendTemplateMessage: 'http://api.gupshup.io/sm/api/v1/template/msg',
+      getWalletBalance: 'https://api.gupshup.io/sm/api/v2/wallet/balance',
+    };
+
+    this.config = {
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        apiKey: this.API_KEY
+      }
+    };
+  };
+
+  /**
+   * 
+   * @param {*} data 
+   * @returns 
+   */
+  getUrlEncodedData = (data: {[key: string]: any}) => {
+    const resultantData = new URLSearchParams();
+    Object.keys(data).forEach((key: string) => {
+      return resultantData.append(key, typeof data[key] === 'object' ? JSON.stringify(data[key]) : data[key]);
+    });
+    return resultantData;
+  };
+
+  /**
+   * 
+   * @returns 
+   */
+  getTemplatesList = async () => await axios.get(this.url.getTemplatesList, this.config);
+
+  getWalletBalance = async () => await axios.get(this.url.getWalletBalance, this.config);
+
+  markUserOptIn = async (userMobileNumber: string) => {
+    const params = this.getUrlEncodedData({
+      user: userMobileNumber
+    });
+
+    return await axios.post(this.url.optInUser, params, this.config);
+  };
+
+  markBulkOptIn = async (userMobileNumbers: string[]) => {
+    const params = this.getUrlEncodedData({
+      users: userMobileNumbers
+    });
+
+    return await axios.post(this.url.bulkOptIn, params, this.config);
+  };
+
+  sendMediaImageMessage = async (userMobileNumber: string, imageUrl: string, caption: string) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      'src.name': this.APP_NAME,
+      message: {
+        type: 'image',
+        originalUrl: imageUrl,
+        previewUrl: imageUrl,
+        caption
+      }
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  sendMediaVideoMessage = async (userMobileNumber: string, videoUrl: string, caption: string) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'video',
+        url: videoUrl,
+        caption,
+      },
+      'src.name': this.APP_NAME
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  sendMediaAudioMessage = async (userMobileNumber: string, audioUrl: string) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'audio',
+        url: audioUrl,
+      },
+      'src.name': this.APP_NAME
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  sendMediaFileMessage = async (userMobileNumber: string, fileUrl: string, filename: string) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'file',
+        url: fileUrl,
+        filename,
+      },
+      'src.name': this.APP_NAME
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+  
+  sendMediaStickerMessage = async (userMobileNumber: string, stickerUrl: string) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'sticker',
+        url: stickerUrl,
+      },
+      'src.name': this.APP_NAME
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  sendTextMessage = async (userMobileNumber: string, message: string) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'text',
+        text: message
+      },
+      'src.name': this.APP_NAME,
+      disablePreview: false
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  sendTemplateMessage = async (
+      userMobileNumber: string,
+      templateId: string,
+      templateParams: string,
+      mediaMessage: string
+    ) => {
+    const params = this.getUrlEncodedData({
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      template: {
+        id: templateId,
+        params: templateParams
+      },
+      message: mediaMessage
+    });
+
+    return await axios.post(this.url.sendTemplateMessage, params, this.config);
+  };
+
+  sendLocation = async (
+      userMobileNumber: string,
+      longitude: string,
+      latitude: string,
+      name: string,
+      address: string
+    ) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'location',
+        longitude,
+        latitude,
+        name,
+        address,
+      },
+      'src.name': this.APP_NAME,
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  sendContactCard = async (userMobileNumber: string, contactCard: ContactCard) => {
+    const params = this.getUrlEncodedData({
+      channel: 'whatsapp',
+      source: this.SOURCE_MOBILE_NUMBER,
+      destination: userMobileNumber,
+      message: {
+        type: 'contact',
+        contact: contactCard,
+      },
+      'src.name': this.APP_NAME,
+    });
+
+    return await axios.post(this.url.sendTextMessage, params, this.config);
+  };
+
+  checkContentType = (type: string, contentType: string) => {
+    const types: {[key: string]: string[]} = {
+      audio: ['audio/aac', 'audio/mp4', 'audio/amr', 'audio/mpeg', 'audio/ogg;codecs=opus'],
+      image: ['image/jpeg', 'image/png'],
+      video: ['video/mp4', 'video/3gpp'],
+    };
+    return types[type].includes(contentType.replace(/ /g,''));
+  }
+
+  checkSize = (type: string, size: number) => {
+    const types: {[key: string]: number} = {
+      image: 5 * 1024 * 1024,  // 5mb
+      audio: 16 * 1024 * 1024, // 16mb      
+      video: 16 * 1024 * 1024, // 16mb
+      file: 100 * 1024 * 1024, // 100mb
+      sticker: 100 * 1024,     // 100kb
+    }
+    return size > 0 && size < types[type];
+  }
+}
+
+module.exports = {
+  GupshupAPIClient,
+}
