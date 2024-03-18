46ae1d05c14e720f1487ca8403e16f578d6079c2
sergey <110@mobilon.ru>
2024-03-18 21:00:27 +0700

    fix

--- typedoc.config.js
+++ typedoc.config.js
@@ -3,7 +3,7 @@ const gitRepoUrl = 'https://github.com/antirek/gupshup-api-client';
 module.exports = {
   entryPoints: [
     "./src/index.ts",
-    "./src/types/index.ts",
+    // "./src/types/index.ts",
   ],
   out: "docs",
   validation: {
@@ -14,4 +14,15 @@ module.exports = {
   sourceLinkTemplate: `${gitRepoUrl}/blob/main/{path}#L{line}`,
   includeVersion: true,
   sourceLinkExternal: true,
+  navigation: {
+    "includeCategories": true,
+    "includeGroups": true,
+    "includeFolders": true
+  },
+  "groupOrder": [
+    "Classes",
+    "Variables",
+    "Functions",
+    "*",
+  ],
 };
